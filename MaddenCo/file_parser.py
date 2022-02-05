b5faffb9bb2296efaec976df1f644abac05901ad
Zak Pruitt <pruittzn@gmail.com>
2022-02-05 16:47:02 -0600

    successfully built information array

--- MaddenCo/file_parser.py
+++ MaddenCo/file_parser.py
@@ -1,20 +1,16 @@
+from cgi import test
+from cgitb import text
+import time
 import re
+from xml.dom.minidom import Element
 
 
 class FileParser:
     def __init__(self, file_path):
         self.file_path = file_path
-        self.emp_flag = 0
+        self.emp_flag = None
         self.email_flag = False
-
-    def __parse_count_line(line):
-        line_elements = line.split()
-        # print(line_elements)
-
-    def __parse_information_line(line):
-        line_elements = line.split()
-        parsed_string = ""
-        print(len(line_elements))
+        self.sub_dept_flag = False
 
     def parse_text(self):
         with open(self.file_path, 'r+', encoding="utf-16") as file:
@@ -22,7 +18,43 @@ class FileParser:
                 if re.search("(^\s|^20([0-9]{6}))", line):
                     if line.startswith(" "):
                         # line with count/email, etc
-                        self.parse_count_line(line)
+                        self.__parse_count_line(line)
+                        # email flag to off
                     else:
                         # line with information
-                        self.parse_information_line(line)
+                        self.__parse_information_line(line)
+
+    def __parse_count_line(self, line):
+        self.email_flag = False
+        line_elements = line.split()
+        # print(line_elements)
+
+    def __parse_information_line(self, line):
+        information_array = self.__build_information_array(line.split())
+
+    def __build_information_array(self, line_array):
+        symbols = ['H', 'S', 'E']
+
+        information_array = ["" for x in range(0, 9)]
+        length = len(line_array)
+
+        for element in line_array:
+            if element in symbols:
+                if self.email_flag == False and element == "E":
+                    self.email_flag = True
+                else:
+                    self.sub_dept_flag = element
+            elif len(element) == 3 and element.isdigit():
+                self.emp_flag = element
+
+        information_array[0] = line_array[0]
+        information_array[1] = line_array[1]
+        information_array[2] = self.sub_dept_flag
+        information_array[3] = self.email_flag
+        information_array[4] = self.emp_flag
+        information_array[5] = line_array[length - 4]
+        information_array[6] = line_array[length - 3]
+        information_array[7] = line_array[length - 2]
+        information_array[8] = line_array[length - 1]
+        
+        print(information_array)
--- MaddenCo/main.py
+++ MaddenCo/main.py
@@ -1,5 +1,6 @@
 from file_parser import FileParser
 
 if __name__ == '__main__':
-    file_parser = FileParser("./MaddenCo/MaddenCo.txt")
+    file_parser = FileParser("./MaddenCo/20220106.txt")
+    file_parser.parse_text()
     print("done")
