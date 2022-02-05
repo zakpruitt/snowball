570b921d0a92ec65ea016ff17f757a186a5604be
Zak Pruitt <pruittzn@gmail.com>
2022-02-05 15:02:39 -0600

    regex for parsing out useless lines

--- MaddenCo/main.py
+++ MaddenCo/main.py
@@ -1,10 +1,17 @@
 import time
+import re
 
 def parse_text(file_name):
     with open(file_name, 'r+', encoding="utf-16") as file:
         for line in file.readlines():
-            if "PAGE" in line:
+            if re.search("(^\s|^20([0-9]{6}))", line):
                 print(line)
+                time.sleep(0.2)
+
+
+
+
+
         
 if __name__ == '__main__':
     parse_text('./MaddenCo/20220106.txt')
