ed11d83cec60fb79ecc9fbb059dbe0f3506ae280
Zak Pruitt <pruittzn@gmail.com>
2022-02-07 21:32:15 -0600

    added initial formatting code

--- MaddenCo/main.py
+++ MaddenCo/main.py
@@ -3,11 +3,29 @@ from file_parser import FileParser
 
 
 def generate_excel_file(data):
+    # create dataframe
     columns = ['Date Created', 'Sup Code', 'Sub Dept', 'Email', 'Employee #',
                'Support #', 'Last User', 'Original User', 'Time Last Changed']
     df = pd.DataFrame(data, columns=columns)
-    writer = pd.ExcelWriter("output.xlsx", engine='xlsxwriter')
-    df.to_excel('output.xlsx', index=False)
+
+    # XLSX writer objects
+    writer = pd.ExcelWriter("output.xlsx",
+                            engine='xlsxwriter',
+                            options={'strings_to_numbers': True})
+    df.to_excel(writer, sheet_name='MaddenCo Data {Date}', index=False)
+    workbook = writer.book
+    worksheet = writer.sheets['MaddenCo Data {Date}']
+
+    # formats
+    red_format = workbook.add_format(
+        {'bg_color': 'white', 'font': 'Calibri Light', 'font_size': '12', 'align': 'center'})
+    green_format = workbook.add_format({'bg_color': 'black'})
+
+    # set columns
+    worksheet.set_column(0, 8, 20, red_format)
+    worksheet.set_column(9, 10, 20, green_format)
+
+    writer.save()
     print(df)
 
 
