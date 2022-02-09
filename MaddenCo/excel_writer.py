851dfa5cba7437c52e33833dff0aeba51ac54c70
Zak Pruitt <pruittzn@gmail.com>
2022-02-09 00:58:02 -0600

    converted plain functions into an iterative OOP approach

---
+++ MaddenCo/excel_writer.py
@@ -0,0 +1,51 @@
+import pandas as pd
+
+
+class ExcelWriter:
+    def __init__(self):
+        self.writer = pd.ExcelWriter("output.xlsx",
+                                     engine='xlsxwriter',
+                                     options={'strings_to_numbers': True})
+        self.workbook = self.writer.book
+        self.formats = {}
+
+    def create_and_write_new_sheet(self, sheet_name, dataframe, row_labels=False):
+        dataframe.to_excel(
+            self.writer,
+            sheet_name=sheet_name,
+            index=self.row_labels
+        )
+
+    def create_format(self, format_name, bg_color, font, font_size, align, border):
+        format = self.workbook.add_format(
+            {'bg_color': bg_color, 'font': font, 'font_size': font_size, 'align': align,
+             border: border})
+        self.formats[format_name] = format
+
+    def format_headers_sf(self, sheet_name, count_df, buffer, format):
+        worksheet = self.writer.sheets[sheet_name]
+        for col_num, value in enumerate(count_df.columns.values):
+            worksheet.write(0, col_num + buffer, value, self.formats[format])
+
+    def format_headers_af(self, sheet_name, count_df, buffer, format1, format2):
+        worksheet = self.writer.sheets[sheet_name]
+        for col_num, value in enumerate(count_df.columns.values):
+            if col_num % 2 == 0:
+                worksheet.write(0, col_num + buffer, value,
+                                self.formats[format1])
+            else:
+                worksheet.write(0, col_num + buffer, value,
+                                self.formats[format2])
+
+    def format_columns_sf(self, sheet_name, dataframe, spacing, format):
+        worksheet = self.writer.sheets[sheet_name]
+        for col in range(0, len(dataframe.columns)):
+            worksheet.set_column(col, col, spacing, self.formats[format])
+
+    def format_columns_af(self, sheet_name, dataframe, spacing, format1, format2):
+        worksheet = self.writer.sheets[sheet_name]
+        for col in range(0, len(dataframe.columns)):
+            if col % 2 == 0:
+                worksheet.set_column(col, col, spacing, self.formats[format1])
+            else:
+                worksheet.set_column(col, col, spacing, self.formats[format2])
--- MaddenCo/main.py
+++ MaddenCo/main.py
@@ -3,12 +3,24 @@ import pandas as pd
 from file_parser import FileParser
 
 
-def write_data_sheet(writer, info_df):
-    # get workbook and sheet
+def generate_excel_file(information, count):
+    # create dataframes
+    columns = ['Date Created', 'Sup Code', 'Sub Dept', 'Email', 'Employee #',
+               'Support #', 'Last User', 'Original User', 'Time Last Changed']
+    info_df = pd.DataFrame(information, columns=columns)
+    count_df = pd.DataFrame(count)
+
+    # XLSX writer objects
+    writer = pd.ExcelWriter("output.xlsx",
+                            engine='xlsxwriter',
+                            options={'strings_to_numbers': True})
+    info_df.to_excel(writer, sheet_name='MaddenCo Data {Date}', index=False)
+    count_df.to_excel(writer, sheet_name='MaddenCo Counts {Date}', index=True)
     workbook = writer.book
     info_worksheet = writer.sheets['MaddenCo Data {Date}']
+    count_worksheet = writer.sheets['MaddenCo Counts {Date}']
 
-    # create formats
+    # formats
     blue_header_format = workbook.add_format(
         {'bg_color': '#5B9BD5', 'font': 'Calibri Light', 'font_size': '12', 'align': 'center',
          'border': 1})
@@ -36,11 +48,6 @@ def write_data_sheet(writer, info_df):
         else:
             info_worksheet.set_column(col, col, 25, yellow_format)
 
-
-def write_count_sheet(writer, count_df):
-    workbook = writer.book
-    count_worksheet = writer.sheets['MaddenCo Counts {Date}']
-
     # set count column headers
     for col_num, value in enumerate(count_df.columns.values):
         if col_num % 2 == 0:
@@ -48,45 +55,16 @@ def write_count_sheet(writer, count_df):
         else:
             count_worksheet.write(0, col_num + 1, value, yellow_header_format)
 
-    # count_worksheet.write(0, 0, "Employee #", blue_header_format)
+    for col_num, value in enumerate(count_df.index.values):
+        count_worksheet.write(0, col_num + 1, value, blue_header_format)
+
+    count_worksheet.write(0, 0, "Employee #", blue_header_format)
     for row in range(0, 4):
         value = count_df.index.values[row]
         count_worksheet.write(row + 1, 0, value, blue_header_format)
 
     for col in range(0, len(count_df.columns) + 1):
         count_worksheet.set_column(col, col, 25, blue_format)
-
-def generate_excel_file(information, count):
-    # create dataframes
-    columns = ['Date Created', 'Sup Code', 'Sub Dept', 'Email', 'Employee #',
-               'Support #', 'Last User', 'Original User', 'Time Last Changed']
-    info_df = pd.DataFrame(information, columns=columns)
-    count_df = pd.DataFrame(count)
-
-    # XLSX writer objects
-    writer = pd.ExcelWriter("output.xlsx",
-                            engine='xlsxwriter',
-                            options={'strings_to_numbers': True})
-    info_df.to_excel(writer, sheet_name='MaddenCo Data {Date}', index=False)
-    count_df.to_excel(writer, sheet_name='MaddenCo Counts {Date}', index=True)
-
-
-    write_data_sheet(writer, info_df)
-
-    # set count column headers
-    # for col_num, value in enumerate(count_df.columns.values):
-    #     if col_num % 2 == 0:
-    #         count_worksheet.write(0, col_num + 1, value, blue_header_format)
-    #     else:
-    #         count_worksheet.write(0, col_num + 1, value, yellow_header_format)
-
-    # # count_worksheet.write(0, 0, "Employee #", blue_header_format)
-    # for row in range(0, 4):
-    #     value = count_df.index.values[row]
-    #     count_worksheet.write(row + 1, 0, value, blue_header_format)
-
-    # for col in range(0, len(count_df.columns) + 1):
-    #     count_worksheet.set_column(col, col, 25, blue_format)
 
     writer.save()
 
