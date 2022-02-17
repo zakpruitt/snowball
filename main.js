fd8c226ab714b87d29da8cc532ac1693b6d501c6
Zak Pruitt <pruittzn@gmail.com>
2022-02-17 17:22:28 -0600

    electron-reload added and menu building

--- main.js
+++ main.js
@@ -1,12 +1,37 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
+require('electron-reload')(__dirname);
 
 
 function createWindow() {
+    // create window
     const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1000,
+        height: 700,
     });
     mainWindow.loadFile("src/templates/index.html");
+    mainWindow.removeMenu();
+    // create menus
+    // let menu = Menu.buildFromTemplate(
+    // [
+    //     {
+    //         label: 'File',
+    //         submenu: [
+    //             {
+    //                 label: 'Quit',
+    //                 click: () => {
+    //                     app.quit();
+    //                 }
+    //             },
+    //             {
+    //                 label: 'Open',
+    //                 click: () => {
+    //                     mainWindow.loadFile("src/templates/index.html");
+    //                 }
+    //             }
+    //         ]
+    //     }
+    // ]);
+    // Menu.setApplicationMenu(menu);
 }
 
 app.whenReady().then(() => {
