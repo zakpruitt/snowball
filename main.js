50e43c50ef11e4b8225d959a358036cf8a1ceec3
Zak Pruitt <pruittzn@gmail.com>
2022-02-28 03:13:41 -0600

    initial cards, navbar, and child process killing

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, Menu, globalShortcut } = require('electron');
 const { PythonShell } = require('python-shell');
+var kill = require('tree-kill');
 
 
 PythonShell.run(__dirname + '/src/main.py', null, function (err, results) {
@@ -16,36 +17,24 @@ function createWindow() {
     mainWindow.loadURL('http://127.0.0.1:5000');
     mainWindow.removeMenu();
     mainWindow.setResizable(false);
-    //mainWindow.webContents.openDevTools();
-    // create menus
-    // let menu = Menu.buildFromTemplate(
-    // [
-    //     {
-    //         label: 'File',
-    //         submenu: [
-    //             {
-    //                 label: 'Quit',
-    //                 click: () => {
-    //                     app.quit();
-    //                 }
-    //             },
-    //             {
-    //                 label: 'Open',
-    //                 click: () => {
-    //                     mainWindow.loadFile("src/templates/index.html");
-    //                 }
-    //             }
-    //         ]
-    //     }
-    // ]);
-    // Menu.setApplicationMenu(menu);
+
+    globalShortcut.register('f5', function () {
+        console.log('Refreshing Electron page...');
+        mainWindow.reload();
+    })
 }
 
-app.whenReady().then(() => {  
+app.whenReady().then(() => {
     createWindow();
 });
 
 app.on('window-all-closed', () => {
     console.log("done");
+    PythonShell.end(function (err,code,signal) {
+        if (err) throw err;
+        console.log('The exit code was: ' + code);
+        console.log('The exit signal was: ' + signal);
+        console.log('finished');
+    });
     app.quit();
 });
