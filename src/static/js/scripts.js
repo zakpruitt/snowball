afafc20ec2a154e82bfc5ce94cfac8c79c1ef08b
Zak Pruitt <pruittzn@gmail.com>
2022-03-27 21:50:12 -0500

    added in filter prototype, flushed out filtering functionality in event handlers, and added in search and date functions

--- src/static/js/scripts.js
+++ src/static/js/scripts.js
@@ -13,18 +13,14 @@ const emailRadioButtons = $(".emailRadioButton")
 $(document).ready(function () {
     sheetTable.dataTable({
         dom: '<<Bft>ip>',
-        // dom: 'lfrtip',
         buttons: [
             {
                 extend: 'excelHtml5',
-                customize: function(xlsx){
+                customize: function (xlsx) {
                     customFormating(xlsx);
                 },
-                
-                
                 text: '<i class="fas fa-file-excel"></i>',
-                className: 'btn btn-outline btn-success'
-                
+                className: 'btn btn-outline btn-success',
             },
             {
                 extend: 'pdf',
@@ -38,34 +34,34 @@ $(document).ready(function () {
     dateConstraint.daterangepicker();
 });
 
-function customFormating(xlsx){
+function customFormating(xlsx) {
     var sheet = xlsx.xl.worksheets['sheet1.xml'];
     var count = 0;
-    var columns = ['A','B','C','D','E','F','G','H','I']
+    var columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
     var skippedHeader = false;
     var skippedTitle = false;
-    for (let i = 0; i<columns.length;i++){
-        $('row c[r*="'+columns[i] +'"]', sheet).each(function(){
-        if (skippedHeader&skippedTitle){
-            if (count % 2 === 0){
-                $(this).attr('s','35');
-            }
-            else{
-                $(this).attr('s','40');
-            }
-            count++;
+    for (let i = 0; i < columns.length; i++) {
+        $('row c[r*="' + columns[i] + '"]', sheet).each(function () {
+            if (skippedHeader & skippedTitle) {
+                if (count % 2 === 0) {
+                    $(this).attr('s', '35');
+                }
+                else {
+                    $(this).attr('s', '40');
+                }
+                count++;
 
-        }
-        else if(skippedTitle){
+            }
+            else if (skippedTitle) {
 
-            skippedHeader=true;
-        }else{
-            skippedTitle=true;
-        }
-    });
-    skippedHeader = false;
-    
-}
+                skippedHeader = true;
+            } else {
+                skippedTitle = true;
+            }
+        });
+        skippedHeader = false;
+
+    }
 }
 
 // PARSE FILE FUNCTIONS 
@@ -90,62 +86,115 @@ dailyFilePath && dailyFilePath.addEventListener("change", (e) => {
 
 // SHEET TABLE FUNCTIONS
 
+function DTFilter() {
+    this.searches = new Array();
+}
+
+DTFilter.prototype.addSearch = function (search) {
+    this.searches.push(search);
+}
+
+DTFilter.prototype.removeSearch = function (search) {
+    this.searches.splice(this.searches.indexOf(search), 1);
+}
+
+DTFilter.prototype.clearSearches = function (search) {
+    this.searches = [];
+}
+
+DTFilter.prototype.buildSearchString = function () {
+    var searchString = "";
+    for (let i = 0; i < this.searches.length; i++) {
+        searchString += this.searches[i].toString();
+        if (i < this.searches.length - 1) {
+            searchString += "|";
+        }
+    }
+    return searchString;
+}
+
+const employeeFilter = new DTFilter();
+const dateFilter = new DTFilter();
+const subDeptFilter = new DTFilter();
+const supCodeFilter = new DTFilter();
+
 employeeSheetChecks && employeeSheetChecks.each(function (i, element) {
     element.addEventListener("change", (e) => {
         if (e.target.checked) {
-            alert('hello');
+            employeeFilter.addSearch(e.target.value);
+            employeeFilter.addSearch(e.target.id);
+        } else {
+            employeeFilter.removeSearch(e.target.value);
+            employeeFilter.removeSearch(e.target.id);
         }
-    })
+        search([5, 6, 7], employeeFilter.buildSearchString());
+        console.log(employeeFilter.buildSearchString());
+    });
+
+    employeeFilter.addSearch(element.value);
+    employeeFilter.addSearch(element.id);
 });
 
-dateConstraint && dateConstraint.each(function (i, element) {
-    element.addEventListener("change", (e) => {
-        if (e.target.checked) {
-            alert('hello');
-        }
-    })
+dateConstraint && dateConstraint.on('apply.daterangepicker', function(ev, picker) {
+    // clear searches and show formatted value on picker
+    dateFilter.clearSearches();
+    $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
+    
+    // get dates from picker
+    var startDate = picker.startDate.format('MM-DD-YYYY').toString();
+    var endDate = picker.endDate.format('MM-DD-YYYY').toString();
+    
+    // get all dates (inclusive) inbetween start and end. loop through them and add to filter.
+    getDates(new Date(startDate), new Date(endDate)).forEach(date => {
+        dateFilter.addSearch(date.toString());
+    });
+
+    // search
+    search(1, dateFilter.buildSearchString());
 });
 
 subDeptChecks && subDeptChecks.each(function (i, element) {
     element.addEventListener("change", (e) => {
         if (e.target.checked) {
-            var table = sheetTable.DataTable();
-            table.column(3).search(e.target.value + "H|S",true,false).draw();
-            // var array = table.column(3)
-            //     .data()
-            //     .filter(function (value, index) {
-            //         return value === e.target.value;
-            //     })
-            //     .draw();
-
-            // $.fn.dataTableExt.afnFiltering.push(
-            //     function (oSettings, aData, iDataIndex) {
-            //         return aData[3] === e.target.value;
-            //     }
-            // );
-
-            // table.draw()
+            subDeptFilter.addSearch(e.target.value);
         } else {
-
+            subDeptFilter.removeSearch(e.target.value);
         }
+        search(3, subDeptFilter.buildSearchString());
     })
 });
 
 supCodeChecks && supCodeChecks.each(function (i, element) {
     element.addEventListener("change", (e) => {
         if (e.target.checked) {
-            var table = sheetTable.DataTable();
-            table.search('').columns(2).search(e.target.value).draw();
+            supCodeFilter.addSearch(e.target.value);
+        } else {
+            supCodeFilter.removeSearch(e.target.value);
         }
+        search(2, supCodeFilter.buildSearchString());
     })
 });
 
 emailRadioButtons && emailRadioButtons.each(function (i, element) {
     element.addEventListener("change", (e) => {
         if (e.target.checked) {
-            alert('hello');
+            search(4, e.target.value);
         }
     })
 });
 
+function search(colNum, searchString) {
+    var table = sheetTable.DataTable();
+    table.column(colNum).search(searchString, true, false).draw();
+}
 
+function getDates(startDate, stopDate) {
+    var dateArray = [];
+    var currentDate = moment(startDate);
+    var stopDate = moment(stopDate);
+    while (currentDate <= stopDate) {
+        dateArray.push( moment(currentDate).format('YYYYMMDD') )
+        currentDate = moment(currentDate).add(1, 'days');
+    }
+    return dateArray;
+}
