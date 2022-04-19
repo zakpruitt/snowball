8607835793a25e1c6dd61c210d68d8ca98e0ce61
Zak Pruitt <pruittzn@gmail.com>
2022-04-19 02:18:27 -0500

    visualize date picker added in and table formatting

--- src/static/js/scripts.js
+++ src/static/js/scripts.js
@@ -203,6 +203,28 @@ $(document).ready(function () {
     hardwareImmPieChart && renderChart(hardwareImmPieChart, 'pie-data?sub_dept=H&category=imm', 'Hardware Immediate Distibutiuon');
     softwareLaterPieChart && renderChart(softwareLaterPieChart, 'pie-data?sub_dept=S&category=later', 'Software Later Distribution');
     hardwareLaterPieChart && renderChart(hardwareLaterPieChart, 'pie-data?sub_dept=H&category=later', 'Hardware Later Distibutiuon');
+
+    var start = moment().subtract(29, 'days');
+    var end = moment();
+
+    function cb(start, end) {
+        $('#visualize-date-picker span').html(start.format('MM/DD/YYYY') + ' - ' + end.format('MM/D/YYYY'));
+    }
+
+    $('#visualize-date-picker').daterangepicker({
+        startDate: start,
+        endDate: end,
+        ranges: {
+           'Today': [moment(), moment()],
+           'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+           'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+           'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+           'This Month': [moment().startOf('month'), moment().endOf('month')],
+           'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        }
+    }, cb);
+
+    cb(start, end);
 });
 
 function renderLineGraph(chart, endpoint, title, dis_legend = true) {
